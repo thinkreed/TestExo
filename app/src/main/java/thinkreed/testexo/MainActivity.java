c6e59395828657a4d419b873be56e4095d2503ce
thinkreed <thinkreed2017@outlook.com>
2017-10-03 10:45:36 +0800

    add useage for library

--- app/src/main/java/thinkreed/testexo/MainActivity.java
+++ app/src/main/java/thinkreed/testexo/MainActivity.java
@@ -1,5 +1,7 @@
 package thinkreed.testexo;
 
+import com.example.audioonlyplayer.AudioOnlyPlayer;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new AudioOnlyPlayer().init(this);
     }
 }
